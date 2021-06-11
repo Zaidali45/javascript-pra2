331f98e32faa31d624b90223ca796780f2c976e2
Zaidali45 <85540324+Zaidali45@users.noreply.github.com>
2021-06-11 00:29:31 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,95 @@
+// var x =10;
+// var b = x++ + ++x + x + --x + x-- - x
+// alert(b);
+        
+// var x = "Enter value";
+// var y = "your value is"
+// var a = +prompt(x,y);
+// var b = 10+a;
+// document.write("The output is" + b)
+
+
+// var age = +prompt("Enter you age" );
+// if (age == 14){
+//     alert("your age is equal")
+// }
+// else if (age < 14){
+//     alert("your age is less")
+// }
+// else{
+//     alert("you age is more")
+// }
+
+// var age = prompt("Enter you age")
+// var gender = prompt("Enter you gender")
+// if(age > 18  ||  gender == "male"){
+//     alert("you are allowed to ride")
+// }
+// else{
+//     alert("you are not allowed")
+// }
+
+// var a = "5";
+// if (a === a){
+//     alert("true")
+// }
+// else{
+//     alert("false")
+// }
+
+
+// let d = 10;
+// let c = 40;
+// var s = d
+
+// console.log(sum = s+c)
+
+
+// var age = prompt("Enter you age")
+//  var gender = prompt("Enter you gender")
+//  if(age >=18  &&  gender == "male")
+//  {
+//      alert("you are allowed to ride")
+// }
+// else{
+//     alert("you are not allowed")
+// }
+
+// var x = 5;
+// var y = 5;
+// var a = 20;
+// var b = 10;
+// if(x+y === a-b){
+//     alert("correct")
+// }
+// else{
+//     alert("incorrect")
+// }
+
+
+
+var percent = +prompt("Enter your percent")
+if(percent >= 80 && percent <= 100 ){
+    alert("Guide A+")
+}
+else if (percent >= 70 && percent <= 80){
+    alert("Guide A")
+}
+else if (percent >= 60 && percent <= 70){
+    alert("Guide B")
+}
+else if (percent >= 50 && percent <= 60){
+    alert("Guide C")
+}
+else if (percent >= 40 && percent <= 50){
+    alert("Guide D")
+}
+else if (percent >= 33 && percent <= 40){
+    alert("Guide E")
+}
+else if (percent >= 0 && percent <= 33){
+    alert("Guide F")
+}
+else("You did not correct percent ")
+
+
